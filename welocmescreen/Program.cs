5de429b761f21cb1adbe663594737649c781f4cc
Hameedullah Asadi <hameedasade20@gmail.com>
2023-06-01 11:41:10 -0700

    Add project files.

---
+++ welocmescreen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+    internal class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, we'are \"accounting\" on you!");
+        Console.ReadLine();
+        }
+    }
+
